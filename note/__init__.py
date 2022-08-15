7a30f67528a6bf56f4d8d43abcae6b6061a75971
sid00100 <otasiddhant@gmail.com>
2022-08-15 21:02:33 +0530

    sinup generates default data

--- note/__init__.py
+++ note/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, db
 
 
 
@@ -10,4 +10,8 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "SECRET"
 
 cred = credentials.Certificate("note/note-app-db3be-firebase-adminsdk-zwm5y-f4d835d6d3.json")
-firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app(cred,{
+    "databaseURL": "https://note-app-db3be-default-rtdb.asia-southeast1.firebasedatabase.app/"
+})
+
+db_ref = db.reference("/")
